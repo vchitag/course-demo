900b48774ba2b8838dca681ca871c12879e83bea
vchitag <vivekrc@gmail.com>
2018-01-13 14:53:26 +0530

    Testing commit

--- src/main/java/com/example/coursedemo/CourseDemoApplication.java
+++ src/main/java/com/example/coursedemo/CourseDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseDemoApplication {
 
-	//Example file
+	//Example file1
 	public static void main(String[] args) {
 		SpringApplication.run(CourseDemoApplication.class, args);
 	}
